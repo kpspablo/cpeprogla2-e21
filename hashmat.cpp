8a47320ada720d6246edd950531d1fd074d16da2
Karla Patricia Pablo <kpspablo@users.noreply.github.com>
2016-09-17 09:32:22 +0800

    Add files via upload

---
+++ hashmat.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int hash,opp;
+   while(cin>>hash>>opp)
+   {
+		if(hash>opp) 
+			cout<<(opp-hash)<<endl;
+		else
+			cout<<(opp-hash)<<endl;
+   }
+}
