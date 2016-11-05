4d6fcdc498ca49fa50de26a36701d67ccc98257a
Karla Patricia Pablo <kpspablo@users.noreply.github.com>
2016-11-05 13:39:40 +0800

    Update fizzbuzz_file.cpp

--- fizzbuzz_file.cpp
+++ fizzbuzz_file.cpp
@@ -6,13 +6,13 @@ int n;
 int main(){
 	ifstream fizzbuzz("input.txt");
 	while(fizzbuzz >> n){	
-		if((n%3==0) && (n%5==0)){
+		if((n%3 == 0) && (n%5 == 0)){
 			cout << "\nFizzBuzz";
 		}
-		else if(n%5==0){
+		else if(n%5 == 0){
 			cout << "\nBuzz";
 		}
-		else if(n%3==0){
+		else if(n%3 == 0){
 			cout << "\nFizz";
 		}
 		else{
