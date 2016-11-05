b6de07b74cffe18fd135e8a34d3006262eea283b
Karla Patricia Pablo <kpspablo@users.noreply.github.com>
2016-11-05 13:44:02 +0800

    Create euler3.cpp

---
+++ euler3.cpp
@@ -0,0 +1,15 @@
+//pablo
+#include<iostream>
+#include<fstream>
+using namespace std;
+int n,x;
+int main(){
+  ifstream eul("input.txt");
+	while(eul >> n){
+		for(int i = 1; i < n; i++)
+		  if(i%3 == 0 || i%5 == 0)
+			  x+ = i;
+	    cout << x << endl;	
+	}
+  system("pause");
+}
